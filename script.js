1952d0a04992ac2e342f023ba6221a430c290cf0
gokhanefekayasaroglu <123803476+gokhanefekayasaroglu@users.noreply.github.com>
2023-03-06 14:31:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function changeContentYes() {
+  // Get the div element by its ID
+  var divElement = document.getElementById("button-56");
+
+  // Change the content of the div element
+  divElement.innerHTML = "aww cutie now dm me<3";
+}
+
+function changeContentNo() {
+  // Get the div element by its ID
+  var divElement = document.getElementById("noButton");
+
+  // Change the content of the div element
+  divElement.innerHTML = "orospu cocugu";
+}
+
